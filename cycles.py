60976a5e1ff8b4a0f40a4f359f60e82dd7665db9
Julian Kuhlmann <jlnkuhlmann@gmail.com>
2017-11-25 14:04:56 -0800

    Clean-up and more, generic tests.

--- cycles.py
+++ cycles.py
@@ -1,27 +1,3 @@
-# def depth_first_search(adjacency_list, node, visited, start, results):
-#     if node in visited:
-#         if node == start:
-#             visited.append(node)
-#             results.append(visited)
-#             visited = list()
-#         return
-#     visited.append(node)
-#     for child in adjacency_list[node]:
-#         depth_first_search(adjacency_list, child, visited, start, results)
-#     return results
-
-# def depth_first_search(graph, start):
-#     results, visited, stack = list(), list(), [start]
-#     while stack:
-#         vertex = stack.pop()
-#         if vertex not in visited:
-#             visited.append(vertex)
-#             stack.extend(v for v in graph[vertex] if v not in visited or v is start)
-#         elif vertex is start:
-#             visited.append(vertex)
-#             results.append(visited)
-#     return results
-
 def get_all_cycles(graph, start):
     stack = [(start, [start])]
     while stack:
@@ -32,7 +8,7 @@ def get_all_cycles(graph, start):
             else:
                 stack.append((next, path + [next]))
 
-def get_all_cycles_of_length_n(graph, start, n_vertices):
+def get_all_full_cycles(graph, start):
     all_cycles = list(get_all_cycles(graph, start))
-    all_cycles_length_n = [cycle for cycle in all_cycles if len(cycle) == n_vertices + 1]
-    return all_cycles_length_n
+    all_full_cycles = [cycle for cycle in all_cycles if len(cycle) == len(graph) + 1]
+    return all_full_cycles
--- tests/test_cycles.py
+++ tests/test_cycles.py
@@ -1,37 +1,78 @@
 import cycles
 
 
-def test_two_person_wichteling_unlimited():
-    julian = 'Julian'
-    paul = 'Paul'
+def test_all_full_cycles_two_nodes_unlimited():
+    node1 = '1'
+    node2 = '2'
     graph = {
-        julian: {paul},
-        paul: {julian}
+        node1: {node2},
+        node2: {node1}
     }
-    result = cycles.get_all_cycles_of_length_n(
+    result = cycles.get_all_full_cycles(
 	graph=graph,
-	start=julian,
-	n_vertices=2
+	start=node1
     )
-    expected = [[julian, paul, julian]]
+    expected = [[node1, node2, node1]]
     assert result == expected
 
 
-def test_three_person_wichteling_unlimited():
-    julian = 'Julian'
-    paul = 'Paul'
-    ulrike = 'Ulrike'
+def test_all_full_cycles_three_nodes_unlimited():
+    node1 = '1'
+    node2 = '2'
+    node3 = '3'
     graph = {
-        julian: {paul, ulrike},
-        paul: {julian, ulrike},
-        ulrike: {paul, julian}
+        node1: {node2, node3},
+        node2: {node1, node3},
+        node3: {node2, node1}
     }
-    result = cycles.get_all_cycles_of_length_n(
+    result = cycles.get_all_full_cycles(
         graph=graph,
-        start=julian,
-	n_vertices=3
+        start=node1
     )
-    assert [julian, paul, ulrike, julian] in result
-    assert [julian, ulrike, paul, julian] in result
+    assert [node1, node2, node3, node1] in result
+    assert [node1, node3, node2, node1] in result
     assert len(result) == 2
+
+
+def test_all_full_cycles_four_nodes_limited():
+    node1 = '1'
+    node3 = '3'
+    node2 = '2'
+    node4 = '4'
+    graph = {
+        node1: {node2, node3},
+        node2: {node1, node4},
+        node3: {node1, node4},
+        node4: {node2, node3}
+    }
+    result = cycles.get_all_full_cycles(
+        graph=graph,
+        start=node1
+    )
+    assert [node1, node2, node4, node3, node1] in result
+    assert [node1, node3, node4, node2, node1] in result
+    assert len(result) == 2
+
+
+def test_all_cycles_three_nodes_unlimited():
+    node1 = '1'
+    node2 = '2'
+    node3 = '3'
+    graph = {
+        node1: {node2, node3},
+        node2: {node1, node3},
+        node3: {node2, node1}
+    }
+    result = list(
+        cycles.get_all_cycles(
+            graph=graph,
+            start=node1
+        )
+    )
+    assert [node1, node2, node3, node1] in result
+    assert [node1, node3, node2, node1] in result
+    assert [node1, node3, node1] in result
+    assert [node1, node2, node1] in result
+    assert len(result) == 4
+
 
