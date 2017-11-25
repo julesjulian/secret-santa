2b97a92afcae4a14cfb28f847547212eee61f5e4
Julian Kuhlmann <jlnkuhlmann@gmail.com>
2017-11-25 14:22:46 -0800

    Implementation with results list and more tests.

--- cycles.py
+++ cycles.py
@@ -1,14 +1,17 @@
 def get_all_cycles(graph, start):
     stack = [(start, [start])]
-    while stack:
+    all_cycles = []
+    while len(stack) > 0:
         (vertex, path) = stack.pop()
-        for next in graph[vertex] - set(path[1:]):
-            if next == start:
-                yield path + [next]
+        children_not_on_path_or_start = graph[vertex] - set(path[1:])
+        for child in children_not_on_path_or_start:
+            if child == start:
+                all_cycles.append(path + [child])
             else:
-                stack.append((next, path + [next]))
+                stack.append((child, path + [child]))
+    return all_cycles
 
 def get_all_full_cycles(graph, start):
-    all_cycles = list(get_all_cycles(graph, start))
+    all_cycles = get_all_cycles(graph, start)
     all_full_cycles = [cycle for cycle in all_cycles if len(cycle) == len(graph) + 1]
     return all_full_cycles
--- tests/test_cycles.py
+++ tests/test_cycles.py
@@ -1,6 +1,46 @@
 import cycles
 
 
+def test_all_cycles_three_nodes_unlimited():
+    node1 = '1'
+    node2 = '2'
+    node3 = '3'
+    graph = {
+        node1: {node2, node3},
+        node2: {node1, node3},
+        node3: {node2, node1}
+    }
+    result = cycles.get_all_cycles(
+            graph=graph,
+            start=node1
+        )
+
+    assert [node1, node2, node3, node1] in result
+    assert [node1, node3, node2, node1] in result
+    assert [node1, node3, node1] in result
+    assert [node1, node2, node1] in result
+    assert len(result) == 4
+
+
+def test_all_cycles_three_nodes_one_way_block():
+    node1 = '1'
+    node2 = '2'
+    node3 = '3'
+    graph = {
+        node1: {node2},
+        node2: {node1, node3},
+        node3: {node2, node1}
+    }
+    result = cycles.get_all_cycles(
+            graph=graph,
+            start=node1
+        )
+
+    assert [node1, node2, node3, node1] in result
+    assert [node1, node2, node1] in result
+    assert len(result) == 2
+
+
 def test_all_full_cycles_two_nodes_unlimited():
     node1 = '1'
     node2 = '2'
@@ -34,10 +74,10 @@ def test_all_full_cycles_three_nodes_unlimited():
     assert len(result) == 2
 
 
-def test_all_full_cycles_four_nodes_limited():
+def test_all_full_cycles_four_nodes_two_way_block():
     node1 = '1'
-    node3 = '3'
     node2 = '2'
+    node3 = '3'
     node4 = '4'
     graph = {
         node1: {node2, node3},
@@ -54,25 +94,25 @@ def test_all_full_cycles_four_nodes_limited():
     assert len(result) == 2
 
 
-def test_all_cycles_three_nodes_unlimited():
+def test_all_full_cycles_four_nodes_one_way_block():
     node1 = '1'
     node2 = '2'
     node3 = '3'
+    node4 = '4'
     graph = {
         node1: {node2, node3},
-        node2: {node1, node3},
-        node3: {node2, node1}
+        node2: {node1, node3, node4},
+        node3: {node1, node2, node4},
+        node4: {node1, node2, node3}
     }
-    result = list(
-        cycles.get_all_cycles(
-            graph=graph,
-            start=node1
-        )
+    result = cycles.get_all_full_cycles(
+        graph=graph,
+        start=node1
     )
-    assert [node1, node2, node3, node1] in result
-    assert [node1, node3, node2, node1] in result
-    assert [node1, node3, node1] in result
-    assert [node1, node2, node1] in result
+    assert [node1, node2, node3, node4, node1] in result
+    assert [node1, node2, node4, node3, node1] in result
+    assert [node1, node3, node2, node4, node1] in result
+    assert [node1, node3, node4, node2, node1] in result
     assert len(result) == 4
 
 
